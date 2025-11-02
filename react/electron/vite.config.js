c8dacf5a1fa365661fb667dec546449947373bcb
iwannatoa <zhyzhibb@126.com>
2025-11-02 23:01:34 +0800

    update

--- react/electron/vite.config.js
+++ react/electron/vite.config.js
@@ -9,12 +9,11 @@ export default defineConfig({
     open: true,
   },
   build: {
-    outDir: 'build', // 保持与 CRA 相同的输出目录
+    outDir: 'build',
     sourcemap: true,
   },
   resolve: {
     alias: {
-      // 如果项目中有路径别名，在这里配置
       '@': path.resolve(__dirname, './src'),
     },
   },
